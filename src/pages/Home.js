69e62427ae9681f9db7745b9433a866cd1f7f7a8
FujiaoZhao <zhaofujiao@hotmail.com>
2022-05-21 17:20:23 -0400

    send order to firebase

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,43 +1,69 @@
 import classes from './Home.module.css';
 import {Fragment} from "react";
 import Card from '../components/UI/Card';
+import {Carousel} from "react-bootstrap";
 
 const Home = () => {
   return (
     <Fragment>
 
-      <div className={classes.title}>
-        <h1>Joy Bakery</h1>
-      </div>
 
-      <div className={classes.description}>
-        <Card className={classes.about_us}>
-          <h2 className={classes.about_us_title}>About Us</h2>
-          <p className={classes.about_us_p}>We make gorgeous and delicious cakes!We make gorgeous and delicious cakes!We
-            make gorgeous and delicious
-            cakes!We make gorgeous and delicious cakes!We make gorgeous and delicious cakes!We make gorgeous and
-            delicious cakes!We make gorgeous and delicious cakes!We make gorgeous and delicious cakes!We make gorgeous
-            and delicious cakes!We make gorgeous and delicious cakes!We make gorgeous and delicious cakes!</p>
-          <br/>
-          <p className={classes.about_us_p}>We love Joy Bakery!We love Joy Bakery!We love Joy Bakery!We love Joy
-            Bakery!We love Joy Bakery!We love Joy
-            Bakery!We love Joy Bakery!We love Joy Bakery!We love Joy Bakery!We love Joy Bakery!We love Joy Bakery!We
-            love Joy Bakery!</p>
-        </Card>
+        <h1 className={classes.title}>Joy Bakery</h1>
 
-        <div className={classes.gallery}>
+      <div className={classes.main}>
+        <Carousel fade className={classes.carousel}>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={require('../image/sweet_table.JPG')}
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>Enjoy Your Day </h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={require('../image/Wedding.JPG')}
+              alt="Second slide"
+            />
 
+            <Carousel.Caption>
+              <h3>Celebrate The Special Day</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={require('../image/strawberry_cake.JPG')}
+              alt="Third slide"
+            />
 
-          <figure className={classes.gallery_item_4}>
-            <img src={require('../image/sweet_table.JPG')} alt='sponge cake' className={classes.gallery_photo}/>
-          </figure>
-          <figure className={classes.gallery_item_1}>
-            <img src={require('../image/Wedding.JPG')} alt='wedding cake' className={classes.gallery_photo}/>
-          </figure>
-          <figure className={classes.gallery_item_5}>
-            <img src={require('../image/strawberry_cake.JPG')} alt='sponge cake' className={classes.gallery_photo}/>
-          </figure>
+            <Carousel.Caption>
+              <h3>Delicious Strawberry Cake</h3>
+              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
 
+        <div className={classes.description}>
+          <Card>
+            <h2 className={classes.about_us_title}>About Us</h2>
+            <p className={classes.about_us_p}>We make gorgeous and delicious cakes!We make gorgeous and delicious
+              cakes!We
+              make gorgeous and delicious
+              cakes!We make gorgeous and delicious cakes!We make gorgeous and delicious cakes!We make gorgeous and
+              delicious cakes!We make gorgeous and delicious cakes!We make gorgeous and delicious cakes!We make gorgeous
+              and delicious cakes!We make gorgeous and delicious cakes!We make gorgeous and delicious cakes!</p>
+            <br/>
+            <p className={classes.about_us_p}>We love Joy Bakery!We love Joy Bakery!We love Joy Bakery!We love Joy
+              Bakery!We love Joy Bakery!We love Joy
+              Bakery!We love Joy Bakery!We love Joy Bakery!We love Joy Bakery!We love Joy Bakery!We love Joy Bakery!We
+              love Joy Bakery!</p>
+          </Card>
         </div>
       </div>
     </Fragment>
